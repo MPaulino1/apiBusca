1d4de8b71d3ae0c21e79624804d76a215757ef1c
Manoel Paulino <mpaulino.me@gmail.com>
2022-12-10 20:46:01 -0300

    up

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
+import { useState } from 'react';
+import api from './services/api';
+
 import './App.css';
+interface Result {
+  Titulo?: any,
+  Link?: any
+}
 
 function App() {
+  const [result, setResult] = useState<Result[]>();
+  const [search, setSearch] = useState('');
+
+  function handleSearch() {
+    api.get('', {
+      params:
+        { search: search }
+    })
+      .then((res) => {
+        const results = res.data;
+        const data = results.Resultados;
+        setResult(data);
+      }).catch((err) => {
+        console.log('Erro ao acessar rota de busca: ', err);
+      })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container-app'>
+
+      <div className='container-title'>
+        <input placeholder="Digite sua busca..." value={search} onChange={e => setSearch(e.target.value)} />
+        <button onClick={handleSearch}>Buscar</button>
+      </div>
+
+      {
+        result ? result.map(e =>
+        (
+          <div className='container-card'>
+            {e.Titulo}
+            <a href={e.Link} target="_blank\">
+              {e.Link}
+            </a>
+          </div>
+        )
+
+        ) : <></>
+      }
+
     </div>
   );
 }
